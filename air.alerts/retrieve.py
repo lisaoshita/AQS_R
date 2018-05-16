158a6653d8c3fb20e2a7f9e3b5a0ecd6b31114e9
Lisa Oshita <l.oshita@yahoo.com>
2018-05-15 17:57:45 -0700

    added: code to check if there are any incoming/failed messages, code to send email

--- air.alerts/retrieve.py
+++ air.alerts/retrieve.py
@@ -1,29 +1,85 @@
-# =======================
-# retrieving message list 
-# =======================
+# =========================================================
+# Script to send an email if there has been recent activity
+# =========================================================
 
 import datetime
 from twilio.rest import Client
+import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+
+# previous day's date
+prev = datetime.date.today() - datetime.timedelta(1)
 
 account = "xx"
 token = "xx"
 client = Client(account, token)
 
-# this method only allows you to grab messages for a single date
-# use a loop to iterate over each day, save messages in a list? - condense list to pandas data frame?
-# twilio dates are in UTC tz
-# subset data to include only failed messages (for now) 
-# if there are actually numbers, send an email
+# retrieve all messages sent and received on the previous date
+messages = client.messages.list(
+                               date_sent = datetime.date(2018, 4, 27)
+                           )
+
+msglist = []
+for msg in messages:
+  info = [msg.direction,
+          msg.status]
+  msglist.append(info)
+
+# convert list to pandas df
+msg_df = pd.DataFrame(msglist,
+                      columns = ['Direction', 'Status'])
+
+# if there are any failed or received messages - send an email
+if (('failed' in msg_df['Status'].unique()) | ('inbound' in msg_df['Direction'].unique())):
+  
+  # NOT WORKING
+  fromaddr = "XX"
+  toaddr = "XX"
+  msg = MIMEMultipart()
+  msg['From'] = fromaddr
+  msg['To'] = toaddr
+  msg['Subject'] = "TEST"
+ 
+  body = "TESTESTESTETSETET" 
+  msg.attach(MIMEText(body, 'plain'))
+ 
+  server = smtplib.SMTP("smtp.mail.yahoo.com",587)
+  server.starttls()
+  server.login(fromaddr, "XX")
+  text = msg.as_string()
+  server.sendmail(fromaddr, toaddr, text)
+  server.quit()
 
 
-# messages = client.messages.list(
-#                                # "date_sent" <= date(2018, 5, 11),
-#                                start_date = date(2018, 4, 27),
-#                                end_date = date(2018, 5, 4)
-#                            )
+
+
+
+# REMOVED:
+# msglist = []
+# for msg in messages:
+#   info = [msg.to, 
+#           msg.from_,
+#           msg.body,
+#           msg.direction,
+#           msg.status,
+#           msg.error_code]
+#   msglist.append(info)
 # 
-# for message in messages:
-#   print(message.direction)
+# # convert list to pandas df
+# msg_df = pd.DataFrame(msglist,
+#                       columns = ['To','From', 
+#                                  'Body', 'Direction', 
+#                                  'Status', 'Error_Code'])
+# 
+# # subsetting to include only messages received + messages failed to send 
+# msg_df = msg_df[(msg_df['Direction'] == 'inbound') | (msg_df['Status'] == 'failed')]
 
-base = datetime.datetime.today()
-print(base)
+
+
+
+
