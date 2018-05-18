e77f4a0e42e0be9f0a55e7fd51130b2dfc26d43e
Lisa Oshita <l.oshita@yahoo.com>
2018-05-17 21:09:18 -0700

    added: code to send email alert

--- air.alerts/retrieve.py
+++ air.alerts/retrieve.py
@@ -3,81 +3,73 @@
 # =========================================================
 
 import datetime
+
 from twilio.rest import Client
+
 import pandas as pd
+
 import smtplib
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+def main(): 
 
+  # previous day's date
+  prev = datetime.date.today() - datetime.timedelta(1)
 
-# previous day's date
-prev = datetime.date.today() - datetime.timedelta(1)
+  account = "xx"
+  token = "xx"
+  client = Client(account, token)
 
-account = "xx"
-token = "xx"
-client = Client(account, token)
+  # retrieve all messages sent and received on the previous date
+  messages = client.messages.list(
+                                 date_sent = prev # datetime.date(2018, 4, 27), (4/24 for no incoming/failed)
+                             )
 
-# retrieve all messages sent and received on the previous date
-messages = client.messages.list(
-                               date_sent = datetime.date(2018, 4, 27)
-                           )
+  # formatting the messages
+  msglist = []
+  for msg in messages:
+    info = [msg.direction,
+            msg.status]
+    msglist.append(info)
 
-msglist = []
-for msg in messages:
-  info = [msg.direction,
-          msg.status]
-  msglist.append(info)
-
-# convert list to pandas df
-msg_df = pd.DataFrame(msglist,
-                      columns = ['Direction', 'Status'])
-
-# if there are any failed or received messages - send an email
-if (('failed' in msg_df['Status'].unique()) | ('inbound' in msg_df['Direction'].unique())):
+  # convert list of messages to pandas data frame
+  msg_df = pd.DataFrame(msglist,
+                        columns = ['Direction', 'Status'])
   
-  # NOT WORKING
-  fromaddr = "XX"
-  toaddr = "XX"
-  msg = MIMEMultipart()
-  msg['From'] = fromaddr
-  msg['To'] = toaddr
-  msg['Subject'] = "TEST"
- 
-  body = "TESTESTESTETSETET" 
-  msg.attach(MIMEText(body, 'plain'))
- 
-  server = smtplib.SMTP("smtp.mail.yahoo.com",587)
-  server.starttls()
-  server.login(fromaddr, "XX")
-  text = msg.as_string()
-  server.sendmail(fromaddr, toaddr, text)
-  server.quit()
+  # if there are no 'failed' or 'inbound' messages, stop here
+  if (('failed' not in msg_df['Status'].unique()) & ('inbound' not in msg_df['Direction'].unique())):
+    
+    return True
+  
+  else: # send email 
+    
+    fromaddr = "xx" # calpoly email
+    toaddr = "xx"
+    password = "xx"
+    
+    # creating the email
+    msg = MIMEMultipart()
+    
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Check Twilio Account - recent activity detected"
+    
+    body = "Recent activity has been detected on the <a href=""https://www.twilio.com/login"">APCD Twilio account.</a> Please log in and check." 
+    
+    msg.attach(MIMEText(body, 'html'))
+    
+    # opens server
+    server = smtplib.SMTP("smtp-mail.outlook.com", port = 587) 
+    # starts server
+    server.starttls() 
+    
+    server.login(fromaddr, password)
+    server.sendmail(fromaddr, toaddr, msg.as_string())
+    server.quit()
 
-
-
-
-
-# REMOVED:
-# msglist = []
-# for msg in messages:
-#   info = [msg.to, 
-#           msg.from_,
-#           msg.body,
-#           msg.direction,
-#           msg.status,
-#           msg.error_code]
-#   msglist.append(info)
-# 
-# # convert list to pandas df
-# msg_df = pd.DataFrame(msglist,
-#                       columns = ['To','From', 
-#                                  'Body', 'Direction', 
-#                                  'Status', 'Error_Code'])
-# 
-# # subsetting to include only messages received + messages failed to send 
-# msg_df = msg_df[(msg_df['Direction'] == 'inbound') | (msg_df['Status'] == 'failed')]
+if __name__ == '__main__':
+    main()
 
 
 
