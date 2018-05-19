16c1d4295af4eea6fe3826a184a51e20d01e6b00
Lisa Oshita <l.oshita@yahoo.com>
2018-05-18 17:01:09 -0700

    final edits

--- air.alerts/retrieve.py
+++ air.alerts/retrieve.py
@@ -2,7 +2,8 @@
 # Script to send an email if there has been recent activity
 # =========================================================
 
-import datetime
+
+import datetime # this might have to be installed with "pip install datetime"
 
 from twilio.rest import Client
 
@@ -12,58 +13,91 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+
+
 def main(): 
 
-  account = "xx"
-  token = "xx"
+  account = "XX"
+  token = "XX"
   client = Client(account, token)
+
 
   # retrieve all messages sent and received on the previous date
   messages = client.messages.list(
-                                 date_sent = datetime.date.today() # prev # datetime.date(2018, 4, 27), (4/24 for no incoming/failed)
+                                 date_sent = datetime.date.today() - datetime.timedelta(1)
                              )
+  
   
   # formatting the messages
   msglist = []
+
+  
   for msg in messages:
-    info = [msg.direction,
-            msg.status]
+    
+    info = [msg.direction, msg.status]
+            
     msglist.append(info)
+
 
   # convert list of messages to pandas data frame
   msg_df = pd.DataFrame(msglist,
                         columns = ['Direction', 'Status'])
-  
+  print(msg_df)
   # if there are no 'failed' or 'inbound' messages, stop here
-  if (('failed' not in msg_df['Status'].unique()) & ('inbound' not in msg_df['Direction'].unique())):
+  if (('failed' not in msg_df['Status'].unique()) & 
+      ('inbound' not in msg_df['Direction'].unique())):
     
     return True
   
-  else: # send email 
-    
-    fromaddr = "xx"
-    toaddr = "xx"
-    password = "xx"
-    
+  else: # send email
+
+    fromaddr = "XX"
+    recipients = ['XX', 'XX']
+    password = "XX"
+
     # creating the email
     msg = MIMEMultipart()
-    
+
     msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Check Twilio Account - recent activity detected"
-    
-    body = "Recent activity has been detected on the <a href=""https://www.twilio.com/login"">APCD Twilio account.</a> Please log in and check." 
-    
-    msg.attach(MIMEText(body, 'html'))
-    
+    msg['To'] = ", ".join(recipients)
+    msg['Subject'] = "Recent activity detected on Twilio account"
+
+    html = """\
+    <html>
+      <body>
+      <p>Incoming messages or messages that have failed to send have been detected
+         on the APCD Twilio account. <strong>Please log in (https://www.twilio.com/login) and
+         check the message log.</strong><br><br> </p>
+      <p> <strong>Instructions for accessing the message log:</strong> <br> <br>
+         1) After logging in, click the circle icon (with three dots in the middle) located on 
+            the far left of the page. <br>
+         2) Click "Programmable SMS" under the "COMMUNICATIONS CLOUD" list. <br>
+         3) Click "View all Message Logs", located next to the "Recent Messages" section. <br>
+         4) Use the options at the top to filter all text activity to the previous day. Use
+            the drop-down list titled "All status", to display all texts that failed to send or 
+            texts received from subscribers. For failed texts, hovering the mouse over "Failed" 
+            in the message entry, displays an error code describing why the text failed to send.<br></p>
+      <p> <strong>Note on failed texts:</strong> <br><br>
+         Texts usually fail to send because subscribers have unsubscribed from the text service 
+         (indicated by error code 21610). If there is a different error code, check here, 
+         https://www.twilio.com/docs/api/errors, for more information.
+      </p>
+    </body>
+    </html>
+    """
+
+    msg.attach(MIMEText(html, 'html'))
+
     # opens server
-    server = smtplib.SMTP("smtp-mail.outlook.com", port = 587) 
+    server = smtplib.SMTP("smtp-mail.outlook.com", port = 587)
     # starts server
-    server.starttls() 
-    
+    server.starttls()
+
     server.login(fromaddr, password)
-    server.sendmail(fromaddr, toaddr, msg.as_string())
+    server.sendmail(fromaddr, recipients, msg.as_string())
     server.quit()
+
+
 
 if __name__ == '__main__':
     main()
